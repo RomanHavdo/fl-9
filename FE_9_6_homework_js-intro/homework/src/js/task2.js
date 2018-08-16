6320c2c037fc620559d8e2e7f4a231f5a11065f8
Roman Havdo <32357324+RomanHavdo@users.noreply.github.com>
2018-08-16 17:28:53 +0300

    Delete output Function and add console.log

--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -3,10 +3,6 @@ const b = Number(prompt('b length='));
 const angle = Number(prompt('angle='));
 const radianToDeg = 180;
 
-let consoleFunc = function(data) {
-    return console.log(data);
-}
-
 let isValid = function(number) {
     if (isNaN(number)) {
         return true;
@@ -20,10 +16,17 @@ let checkZeros = function(number) {
     return number;
 }
 if (isValid(a) || isValid(b) || isValid(angle)) {
-    consoleFunc('Invalid data');
+    console.log('Invalid data');
 }else {
     const c = Number(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) - 2 * a * b * Math.cos(Math.PI * angle / radianToDeg)));
-    consoleFunc('c length:  ' + checkZeros(c));
-    consoleFunc('Triangle square: ' + checkZeros(Math.round(1 / 2 * a * b * Math.sin(Math.PI * angle / radianToDeg))));
-    consoleFunc('Triangle perimeter:  ' + checkZeros(a + b + c));
+    const triagleSquare=Math.round(1 / 2 * a * b * Math.sin(Math.PI * angle / radianToDeg));
+    if(c<=0 || triagleSquare<=0){
+        console.log('Invalid data');
+    }else{
+       console.log(` 
+        c length: ${+checkZeros(c)} 
+        Triangle square: ${+checkZeros(triagleSquare)}
+        Triangle perimeter: ${+checkZeros(a + b + c)}
+    `); 
+    }
 }
