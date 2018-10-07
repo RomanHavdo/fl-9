fa7448c69e96dcf31e252239d1f5e7b6dee52cc5
Roman Havdo <32357324+RomanHavdo@users.noreply.github.com>
2018-10-07 20:44:58 +0300

    Format code

--- FL_9_5_homework_es-next/homework/tower-of-babel/modules_with_name.js
+++ FL_9_5_homework_es-next/homework/tower-of-babel/modules_with_name.js
@@ -1,9 +1,8 @@
-
 var arg1 = process.argv[2];
 var arg2 = process.argv[3];
-import {PI} from './Math';
-import {sqrt} from './Math';
-import {square} from './Math';
+import { PI } from './Math';
+import { sqrt } from './Math';
+import { square } from './Math';
 
 console.log(PI);
 console.log(sqrt(+arg1));
