5df65ed1e404b912cbb8dc9b8885f4974fc12763
Roman Havdo <32357324+RomanHavdo@users.noreply.github.com>
2018-10-07 20:41:50 +0300

    Format code

--- FL_9_5_homework_es-next/homework/tower-of-babel/arrow_function.js
+++ FL_9_5_homework_es-next/homework/tower-of-babel/arrow_function.js
@@ -1,4 +1,4 @@
 var inputs = process.argv.slice(2);
 var result = inputs.map(x => x[0].toUpperCase())
-    .reduce((result,x) => result+x);
+    .reduce((result, x) => result + x);
 console.log(result);
