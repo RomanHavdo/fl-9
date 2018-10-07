af70c43aacd6372c9f18324ff046a03e58e0a181
Roman Havdo <32357324+RomanHavdo@users.noreply.github.com>
2018-10-07 20:48:12 +0300

    Format code

--- FL_9_5_homework_es-next/homework/learn-generators/Look_sync_do_async.js
+++ FL_9_5_homework_es-next/homework/learn-generators/Look_sync_do_async.js
@@ -1,24 +1,24 @@
 var fs = require('fs');
 
-    function run (generator) {
-      var it = generator(go);
+function run(generator) {
+  var it = generator(go);
 
-      function go (err, result) {
-        if (err) return it.throw(err);
-        it.next(result);
-      }
+  function go(err, result) {
+    if (err) return it.throw(err);
+    it.next(result);
+  }
 
-      go();
-    }
+  go();
+}
 
-    run(function* (done) {
-      var firstFile;
-      try {
-        var dirFiles = yield fs.readdir('NoNoNoNo', done); // No such dir
-        firstFile = dirFiles[0];
-      } catch (err) {
-        firstFile = null;
-      }
+run(function* (done) {
+  var firstFile;
+  try {
+    var dirFiles = yield fs.readdir('NoNoNoNo', done); // No such dir
+    firstFile = dirFiles[0];
+  } catch (err) {
+    firstFile = null;
+  }
 
-      console.log(firstFile);
-    });
+  console.log(firstFile);
+});
